d9ea624827c6529a89e4080d29c969a5bfdc2b5c
Erick <ecanals07@gmail.com>
2020-02-22 12:08:10 -0500

    added h1

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -3,6 +3,7 @@ import User from './User.jsx';
 
 export default ({ users }) => (
   <div className="user-list">
+    <h1>Fake People</h1>
     <p className="lead">
       Here's a list of {users.length} completely fictitious people:
     </p>
